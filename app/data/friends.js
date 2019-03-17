556c76ee4890c62eeb78dbeb1ad694c69d0ed01f
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-17 09:07:00 -0500

    friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+const friends = [
+    {
+        name: "Jonah Simms",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiwi_-7rInhAhVHiqwKHQfPDywQjRx6BAgBEAU&url=http%3A%2F%2Fwww.fanpop.com%2Fclubs%2Fsuperstore%2Fimages%2F39171368%2Ftitle%2F1x02-magazine-profile-jonah-photo&psig=AOvVaw3nhFZoHbtSNZzx0xcLWg2M&ust=1552917969324400",
+        scores: [3, 3, 1, 2, 2, 1, 2, 4, 1, 1]
+    },
+
+    {
+        name: "Amy Sosa",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ89pn8gwAm8URq42NRWZfhtKRBCaTdp9Y3iperLwb_lE9k5kU",
+        scores: [2, 3, 2, 5, 5, 4, 3, 2, 4, 6]
+    },
+    {
+        name: "Mateo Santos",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi_-NO3qYnhAhUHna0KHXpRCaYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.usmagazine.com%2Fentertainment%2Fnews%2Fsuperstores-nico-santos-mateo-would-drive-me-nuts-in-real-life-w507355%2F&psig=AOvVaw3wBo3GfNMuyy9qRSnQNuFU&ust=1552917126567610",
+        scores:[2, 1, 3, 2, 3, 4, 1, 1, 5, 4]
+    },
+    {
+        name: "Cheyenne Lee",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj1nsTbqYnhAhUHiqwKHVebDUUQjRx6BAgBEAU&url=https%3A%2F%2Fwww.usmagazine.com%2Fentertainment%2Fnews%2Fsuperstores-nichole-bloom-teases-cheyenne-and-bos-future-qa%2F&psig=AOvVaw3QYrDHCR6TavacYC42twwy&ust=1552917246921562",
+        scores: [1, 1, 2, 4, 1, 3, 3, 5, 5, 1]
+    },
+    {
+        name: "Dina Fox",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwip4Jv8qYnhAhVSaq0KHdVsDgIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.abqjournal.com%2F1070311%2Flauren-ash-opens-up-about-third-season-of-superstore.html&psig=AOvVaw2CsApnrluM5GgTPAmGayRW&ust=1552917319058051",
+        scores: [1,5,4,5,2,3,2,5,1,3]
+        
+    },
+    {
+        name: "Garrett McNeill",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi4ioyqqonhAhUOi6wKHeh5BisQjRx6BAgBEAU&url=https%3A%2F%2Fgiphy.com%2Fgifs%2Fsuperstore-colton-dunn-26uffjvBcC9ARymzK&psig=AOvVaw37mZ0LymJ-jiZ_2PHQsSFW&ust=1552917409184649",
+        scores: [3,3,1,5,5,2,2,1,1,2]
+        
+
+        
+
+    },
+]
