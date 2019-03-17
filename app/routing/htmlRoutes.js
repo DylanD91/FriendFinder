b6b5fe201c6e6b787b17fde95f161fa648170b1c
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-17 10:08:52 -0500

    update

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+const path = require("path");
+
+//Routing 
+module.exports = function (app) {
+
+    // HTML Get Requests
+   
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
