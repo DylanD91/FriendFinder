048459abd8b484beaa64d28bc4a10ca3a1492135
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-17 10:00:52 -0500

    api routes updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,40 @@
-// Friends Data
-let friendsData = require('../data/friends');
+// Load data 
+var friendsData = require("../data/friends");
+var arraySort = require('array-sort');
 
-module.exports = function(app){
-    //direct the user to json data we have
-    app.get('/api/friends',function(req,res){
-        res.json(friendsData);
-    })
-}; 
+//Routing
+module.exports = function (app) {
 
-// Update Data with Post
-app.post("/api/friends", function (req, res){
-    console.log('friend post data',req.body);
-    // Declare a variable for our best match
-    let bestMatch = {
-        "name": "",
-        "photo": "",
-        "difference": Infinity
-    };
-})
+    //API GET request
+  app.get("/api/Friends", function (req, res) {
+        res.json(friendsData)
+    });
+
+    // API POST Requests
+    app.post("/api/friends", function (req, res) {
+
+        // request body store in one variable
+        var userinputdata = (req.body);
+
+      
+        // for loop for frinddata and userdata
+        var results = 0;
+        var scoretotaldiff = [];
+        for (var i = 0; i < friendsData.length; i++) {
+            results = 0;
+            for (var j = 0; j < 10; j++) {
+                results += Math.abs(userinputdata.scores[j] - friendsData[i].scores[j]);
+            }
+            scoretotaldiff.push({ name: friendsData[i].name, photo: friendsData[i].photo, score: Math.abs(results) });
+        }
+
+        arraySort(scoretotaldiff, 'score');
+     
+
+        // data push in friend.js file
+        friendsData.push(req.body);
+        
+        // return data in survey.html 
+        return res.json(scoretotaldiff[0]);
+    });
+}
